d0271efb0f4ae2f02e4af4547a5c0340b0263f0f
Jake Bennett <bennettjj@s.dcsdk12.org>
2015-09-02 17:08:35 +0100

    task1

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,21 @@
 module MethodsQuiz
 	
 	# TODO - write has_teen?
+
+	def has_teen(a,b,c)
+		if a > 13 && a < 19
+			age1 = true
+	 elsif b>13 && b < 19
+			age2 = true
+   elsif c>13 && c<19
+   		age3 = true
+   end
+   if age1= true || age2 = true || age3 = true
+   		return true
+   else
+   		return false
+		end
+	end
 	
 	# TODO - write not_string
 	
--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -13,4 +13,11 @@ class MethodsQuizTest < MiniTest::Test
 
 	# TODO - write tests here
 
+	def test_has_teen
+    assert_equal true, @m.has_teen(14, 12, 10)
+    assert_equal true, @m.has_teen(9, 12, 15)
+    assert_equal true, @m.has_teen(1, 13, 10)
+    assert_equal true, @m.has_teen(19, 12, 10)
+  end
+
 end
